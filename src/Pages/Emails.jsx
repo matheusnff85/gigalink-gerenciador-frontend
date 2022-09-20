bea98fbfaf74ffe3f6bac1dafd22bd5f1f283962
matheusnff85 <math.marinho154@gmail.com>
2022-09-20 09:54:11 -0300

    feat: implementa requisicao get com o axios para os emails

--- src/Pages/Emails.jsx
+++ src/Pages/Emails.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Emails() {
+  const [emails, setEmails] = useState([]);
+
+  async function getEmails() {
+    const apiData = await axios.get('http://localhost:3001/emails').then((res) => res.data);
+    return setEmails(apiData);
+  };
+
+  useEffect(() => {
+    getEmails();
+  }, []);
+
   return(
     <>
       <h1>Gerenciar Emails</h1>
